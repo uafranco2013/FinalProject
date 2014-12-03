5750e590709adcf3ccf38003c2811e74a9f12cbc
Ubaldo Franco <uafranco.2013@jcmail.jcsu.edu>
2014-12-03 13:47:17 -0500

    Gui Creation

--- gen/com/example/finalproject/R.java
+++ gen/com/example/finalproject/R.java
@@ -25,7 +25,12 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080005;
+        public static final int buttonForgotPassword=0x7f080004;
+        public static final int buttonSignIn=0x7f080002;
+        public static final int buttonSignUp=0x7f080003;
+        public static final int editTextPassword=0x7f080001;
+        public static final int editTextUserName=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -36,7 +41,12 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int forgot_password=0x7f050007;
         public static final int hello_world=0x7f050001;
+        public static final int password=0x7f050004;
+        public static final int sign_in=0x7f050005;
+        public static final int sign_up=0x7f050006;
+        public static final int username=0x7f050003;
     }
     public static final class style {
         /** 
