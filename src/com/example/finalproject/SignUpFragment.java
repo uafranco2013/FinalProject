71982d0bf54e10ed68b10e4c9913dfaa6adef44d
Ubaldo Franco <uafranco.2013@jcmail.jcsu.edu>
2014-12-07 22:41:31 -0500

    Successful Sign Up Screen

--- src/com/example/finalproject/SignUpFragment.java
+++ src/com/example/finalproject/SignUpFragment.java
@@ -1,17 +1,26 @@
 package com.example.finalproject;
 
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
+
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * A simple {@link Fragment} subclass.
  * 
  */
+
 public class SignUpFragment extends Fragment {
 
+	View view;
+	EditText username,password,email,name,address,gender,birthday;
 	public SignUpFragment() {
 		// Required empty public constructor
 	}
@@ -19,8 +28,41 @@ public class SignUpFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_sign_up, container, false);
+		
+		view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+		username = (EditText) view.findViewById(R.id.editTextSUUsername);
+		password = (EditText) view.findViewById(R.id.editTextSUPassword);
+		email = (EditText) view.findViewById(R.id.editTextEmail);
+		name = (EditText) view.findViewById(R.id.editTextName);
+		address = (EditText) view.findViewById(R.id.editTextAddress);
+		gender = (EditText) view.findViewById(R.id.editTextGender);
+		birthday = (EditText) view.findViewById(R.id.editTextBirthday);
+		
+		view.findViewById(R.id.buttonSubmit).setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				ParseUser user = new ParseUser();
+				user.setUsername(username.getText().toString());
+				user.setPassword(password.getText().toString());
+				user.setEmail(email.getText().toString());
+				user.put("name", name.getText().toString());
+				user.put("address", address.getText().toString());
+				user.put("gender", gender.getText().toString());
+				user.put("birthday", birthday.getText().toString());
+				user.signUpInBackground(new SignUpCallback() {
+					  public void done(ParseException e) {
+					    if (e == null) {
+					    	Toast.makeText(getActivity(), "Success", Toast.LENGTH_SHORT).show();
+					    } else {
+					      Toast.makeText(getActivity(), "Error", Toast.LENGTH_SHORT).show();
+					    }
+					  }
+					});
+			}
+		});
+		
+		return view;
 	}
 
 }
