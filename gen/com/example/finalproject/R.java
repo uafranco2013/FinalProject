33d029d2a31299b1fd51dc00c52f2167e5ca2c4a
Ubaldo Franco <uafranco.2013@jcmail.jcsu.edu>
2014-12-07 23:06:00 -0500

    Sign In/Up Functionality

--- gen/com/example/finalproject/R.java
+++ gen/com/example/finalproject/R.java
@@ -43,12 +43,14 @@ public final class R {
         public static final int fragLayout=0x7f080000;
     }
     public static final class layout {
-        public static final int activity_main=0x7f030000;
-        public static final int fragment_sign_in=0x7f030001;
-        public static final int fragment_sign_up=0x7f030002;
+        public static final int activity_core=0x7f030000;
+        public static final int activity_main=0x7f030001;
+        public static final int fragment_sign_in=0x7f030002;
+        public static final int fragment_sign_up=0x7f030003;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int core=0x7f070000;
+        public static final int main=0x7f070001;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
@@ -65,6 +67,7 @@ public final class R {
         public static final int sign_in=0x7f050005;
         public static final int sign_up=0x7f050006;
         public static final int submit=0x7f05000e;
+        public static final int title_activity_core=0x7f05000f;
         public static final int username=0x7f050003;
     }
     public static final class style {
