6d377fffb7e04aef0e4123de48f2df45a1b2a348
FRCerrillo2012 <FRCerrillo.2012@jcmail.jcsu.edu>
2014-12-09 14:19:52 -0500

    Messages Screen
    Displays Contact name and all messages with them, as well as an editText
    and button to reply.

--- gen/com/example/finalproject/R.java
+++ gen/com/example/finalproject/R.java
@@ -25,39 +25,48 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int RelativeLayout1=0x7f080003;
-        public static final int action_settings=0x7f080011;
-        public static final int buttonForgotPassword=0x7f080008;
-        public static final int buttonSignIn=0x7f080006;
-        public static final int buttonSignUp=0x7f080007;
-        public static final int buttonSubmit=0x7f080010;
-        public static final int editTextAddress=0x7f08000d;
-        public static final int editTextBirthday=0x7f08000f;
-        public static final int editTextEmail=0x7f08000b;
-        public static final int editTextGender=0x7f08000e;
-        public static final int editTextName=0x7f08000c;
-        public static final int editTextPassword=0x7f080005;
-        public static final int editTextSUPassword=0x7f08000a;
-        public static final int editTextSUUsername=0x7f080009;
-        public static final int editTextUsername=0x7f080004;
+        public static final int RelativeLayout1=0x7f080007;
+        public static final int action_settings=0x7f080015;
+        public static final int buttonForgotPassword=0x7f08000c;
+        public static final int buttonReply=0x7f080004;
+        public static final int buttonSignIn=0x7f08000a;
+        public static final int buttonSignUp=0x7f08000b;
+        public static final int buttonSubmit=0x7f080014;
+        public static final int editTextAddress=0x7f080011;
+        public static final int editTextBirthday=0x7f080013;
+        public static final int editTextEmail=0x7f08000f;
+        public static final int editTextGender=0x7f080012;
+        public static final int editTextName=0x7f080010;
+        public static final int editTextPassword=0x7f080009;
+        public static final int editTextReplyMessage=0x7f080005;
+        public static final int editTextSUPassword=0x7f08000e;
+        public static final int editTextSUUsername=0x7f08000d;
+        public static final int editTextUsername=0x7f080008;
         public static final int fragLayout=0x7f080001;
         public static final int fragmentTabLayout=0x7f080000;
-        public static final int listViewUsers=0x7f080002;
+        public static final int listViewMessages=0x7f080003;
+        public static final int listViewUsers=0x7f080006;
+        public static final int textViewContactName=0x7f080002;
     }
     public static final class layout {
         public static final int activity_core=0x7f030000;
         public static final int activity_main=0x7f030001;
-        public static final int fragment_all_users=0x7f030002;
-        public static final int fragment_following=0x7f030003;
-        public static final int fragment_messages=0x7f030004;
-        public static final int fragment_sign_in=0x7f030005;
-        public static final int fragment_sign_up=0x7f030006;
+        public static final int activity_messages=0x7f030002;
+        public static final int activity_profile_settings=0x7f030003;
+        public static final int fragment_all_users=0x7f030004;
+        public static final int fragment_following=0x7f030005;
+        public static final int fragment_messages=0x7f030006;
+        public static final int fragment_sign_in=0x7f030007;
+        public static final int fragment_sign_up=0x7f030008;
     }
     public static final class menu {
         public static final int core=0x7f070000;
         public static final int main=0x7f070001;
+        public static final int messages=0x7f070002;
+        public static final int profile_settings=0x7f070003;
     }
     public static final class string {
+        public static final int Reply_btn_str=0x7f050012;
         public static final int action_settings=0x7f050002;
         public static final int address=0x7f05000b;
         public static final int app_name=0x7f050000;
@@ -69,10 +78,13 @@ public final class R {
         public static final int hello_world=0x7f050001;
         public static final int name=0x7f05000d;
         public static final int password=0x7f050004;
+        public static final int reply_str=0x7f050011;
         public static final int sign_in=0x7f050005;
         public static final int sign_up=0x7f050006;
         public static final int submit=0x7f05000e;
         public static final int title_activity_core=0x7f05000f;
+        public static final int title_activity_messages=0x7f050010;
+        public static final int title_activity_profile_settings=0x7f050013;
         public static final int username=0x7f050003;
     }
     public static final class style {
