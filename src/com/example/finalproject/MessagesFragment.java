ad310bc2834baf311ea751f84b65172f08a9dec4
Ubaldo Franco <uafranco.2013@jcmail.jcsu.edu>
2014-12-10 01:25:46 -0500

    Revert 3f49934..56445ff
    This rolls back to commit 3f49934aeee1007a86e0d9f7a96f51cd0af895aa.

--- src/com/example/finalproject/MessagesFragment.java
+++ src/com/example/finalproject/MessagesFragment.java
@@ -1,16 +1,10 @@
 package com.example.finalproject;
 
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseQueryAdapter;
-import com.parse.ParseUser;
-
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,25 +20,7 @@ public class MessagesFragment extends Fragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 		// Inflate the layout for this fragment
-		View view = inflater.inflate(R.layout.fragment_messages, container, false);
-		ListView listView = (ListView) view.findViewById(R.id.listViewMsgs);
-		
-		ParseQueryAdapter<ParseObject> adapter =
-				  new ParseQueryAdapter<ParseObject>(getActivity(), new ParseQueryAdapter.QueryFactory<ParseObject>() {
-				    public ParseQuery<ParseObject> create() {
-				      // Here we can configure a ParseQuery to our heart's desire.
-				      ParseQuery query = new ParseQuery("Messages");
-				      query.whereEqualTo("sender", ParseUser.getCurrentUser());
-				      return query;
-				    }
-				  });
-		
-		adapter.setTextKey("message");
-		listView.setAdapter(adapter);
-		
-		
-		
-		return view;
+		return inflater.inflate(R.layout.fragment_messages, container, false);
 	}
 
 }
